a6045db4e083a9df4b6472d1b1f08fdae1bfea7a
Leandro Sabus <leandro.sabus@gmail.com>
2023-01-30 18:18:36 -0300

    theme add

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'tittle': '#1e2022',
+      'subtitle': '#677788',
+      'cards-tittle': '#001b4d',
+      'bg-footer': '#001b4d',
+      'buttons': '377dff',
+      'text-orange': 'ea5e60',
+    },
   },
   plugins: [],
 }
